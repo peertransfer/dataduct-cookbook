d039e4cb2a5d9dd5b58e8ecfc5ccb7b543a5b2fc
Vicente Dominguez <vicendominguez@users.noreply.github.com>
2016-06-23 16:07:39 +0000

    Bump version to 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'engineering@flywire.com'
 license          'All rights reserved'
 description      'Installs/Configures dataduct'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'build-essential'
 depends 'poise-python', '~> 1.4'
