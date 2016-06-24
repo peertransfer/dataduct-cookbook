1ed7ba56febc803145538333c17e297e3f036236
Jose Luis Salas <josacar@users.noreply.github.com>
2016-06-24 09:53:26 +0000

    Add source_url and issues_url

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'berkshelf'
 gem 'test-kitchen'
 gem 'kitchen-inspec'
 gem 'kitchen-dokken'
+gem 'foodcritic'
 
 group :release do
   gem 'stove'
--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,8 @@ description      'Installs/Configures dataduct'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.1'
 
+issues_url 'https://github.com/peertransfer/dataduct-cookbook/issues'
+source_url 'https://github.com/peertransfer/dataduct-cookbook'
+
 depends 'build-essential'
 depends 'poise-python', '~> 1.4'
