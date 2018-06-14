ad3259461e32d2aee0d0c83d6e628c01c4d1b473
Jose Luis Salas <josacar@users.noreply.github.com>
2018-06-14 14:03:53 +0200

    [SREFTP-596] Add sns topics configuration to resource

--- Gemfile
+++ Gemfile
@@ -1,9 +1,10 @@
 source 'https://rubygems.org'
-gem 'chefspec', '~> 4.7.0', require: false
-gem 'foodcritic', '~> 7.0', require: false
+
+gem 'chefspec', '~> 7.2', require: false
+gem 'foodcritic', '~> 13.1', require: false
 gem 'kitchen-inspec', require: false
 gem 'test-kitchen', require: false
 gem 'kitchen-dokken', require: false
-gem 'berkshelf', '~> 4.3', require: false
+gem 'berkshelf', '~> 7.0', require: false
 
 gem 'stove'
---
+++ spec/providers/dataduct_spec.rb
@@ -0,0 +1,24 @@
+require_relative '../spec_helper'
+
+describe 'dataduct-test::default' do
+  cached(:subject) do
+    ChefSpec::SoloRunner.new(step_into: %w(dataduct)).converge(described_recipe)
+  end
+  let(:node) { subject.node }
+
+  context 'dataduct' do
+    it { is_expected.to include_recipe('build-essential') }
+
+    it { is_expected.to install_package('libmysqlclient-custom-dev') }
+    it { is_expected.to install_package('libpq-custom-dev') }
+
+    it 'installs python using poise-python cookbook' do
+      is_expected.to install_python_runtime('3')
+    end
+
+    it 'installs requests and dataduct python packages' do
+      is_expected.to install_python_package('requests')
+      is_expected.to install_python_package('dataduct')
+    end
+  end
+end
